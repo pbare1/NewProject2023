54316c333b7e85c546e6e9b7b678977e38c83ff7
Pasko Bare <pasko.bare@globallogic.com>
2023-12-08 09:16:15 -0500

    first commit

---
+++ slova.py
@@ -0,0 +1,5 @@
+#!/bin/sh
+print("**************************************")
+print("*** THIS IS DEMO FOR JENKINS *********")
+print("*******  123456  *********************")
+print("*****    trigger by webhook  *********")
